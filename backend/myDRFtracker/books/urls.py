aa2c7861fb75f7b3aced59fa9578b207092b1a60
toffy <oketofoke@gmail.com>
2020-06-20 02:35:17 +0100

    removed trailing unecessary slsh

--- backend/myDRFtracker/books/urls.py
+++ backend/myDRFtracker/books/urls.py
@@ -3,6 +3,6 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from books import views
 
 urlpatterns = [
-    path('/', views.BookList.as_view()),
+    path('', views.BookList.as_view()),
     path('<int:pk>/', views.BookDetail.as_view()),
 ]
